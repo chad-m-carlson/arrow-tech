1753ac75508450f3975a8151b19ebab39e6d6373
Chad <chad.m.carlson@gmail.com>
2019-09-27 11:48:36 -0600

    pdf started, fixed calibration form so enter key will tab through form

--- app/models/dosimeter.rb
+++ app/models/dosimeter.rb
@@ -2,5 +2,5 @@ class Dosimeter < ApplicationRecord
   belongs_to :customer
   has_many :calibrations
 
-  validates :serial_numbner, uniquness: { case_sensitive: false }
+  # validates :serial_numbner, uniquness: { case_sensitive: false }
 end
