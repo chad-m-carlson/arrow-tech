6275c1d865526065fd2d2201b01597f58d600ee1
Chad <chad.m.carlson@gmail.com>
2019-11-14 12:54:17 -0700

    login error message added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   mount_devise_token_auth_for 'User', at: 'api/auth'
-
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'}
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   if Rails.env.development?
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
