3faf4999e95f25df87ff5db75e70b38cb291d621
Chad <chad.m.carlson@gmail.com>
2020-02-09 15:58:41 -0700

    dosimeter can now change customer ownership

--- app/models/dosimeter.rb
+++ app/models/dosimeter.rb
@@ -3,6 +3,6 @@ class Dosimeter < ApplicationRecord
   has_many :calibrations
 
   validates :model_number, :serial_number, :range, presence: true
-  validates :serial_number, uniqueness: { case_sensitive: false, scope: :model_number }
+  validates :serial_number, uniqueness: { case_sensitive: false, scope: [:model_number, :customer_id] }
   validates :customer_id, presence: {message: "Must Select a customer"}
 end
